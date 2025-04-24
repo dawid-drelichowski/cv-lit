b46db78ca46ef1eedb932f006bb5b7fbd75411c1
Dawid Drelichowki <dawid.drelichowski@gmail.com>
2025-04-24 20:38:27 +0200

    Percy visual tests integration

--- tests/cv.spec.ts
+++ tests/cv.spec.ts
@@ -1,9 +1,11 @@
 import {test, expect} from '@playwright/test';
+import percySnapshot from '@percy/playwright';
 
 test.describe('CV', () => {
   test('displays properly', async ({page}) => {
     await page.goto('http://localhost:4173/');
 
     await expect(page).toHaveTitle('CV - Dawid Drelichowski');
+    await percySnapshot(page, 'CV');
   });
 });
